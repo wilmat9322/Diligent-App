5a933a298d67e6a16fc624f55ce3585693c46dd7
wilmat9322 <77747554+wilmat9322@users.noreply.github.com>
2021-02-04 13:59:28 -0400

    Add files via upload

---
+++ DiligentApp-repo/authenticate-first.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Diligent Technologies</title>
+  </head>
+  <body>
+    <?php echo "hello world"; ?>
+  </body>
+</html>
