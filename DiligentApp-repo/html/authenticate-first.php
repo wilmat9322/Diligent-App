d68a819f1f342de59cfd3e77f943b6b3bab8bf6f
wilmat9322 <prisa15.matos@gmail.com>
2021-02-08 20:32:16 -0400

    Añadí código del login-box

--- DiligentApp-repo/html/authenticate-first.php
+++ DiligentApp-repo/html/authenticate-first.php
@@ -6,7 +6,23 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
   <body>
-    <div class="login-box">
-    <img src ="images/user.png" class = "avatar">
-  </body>
+  <div class="login-box">
+  <img src ="images/user.png" class = "avatar">
+    <h1>Welcome to Diligent Technologies</h1>
+      <form>
+        <p>Company Code</p>
+        <input type="text" name="username" placeholder="Enter Employee Code">
+        <p>Employee Code</p>
+        <input type="text" name="username" placeholder="Enter Employee Code">
+        <p>Password</p>
+        <input type="password" name="password" placeholder="Enter Password">
+        <input type="submit" name="submit" value="Login">
+        <a href="#">Forgot Password?</a>
+        <a href="#">Register here!</a>
+
+      </form>
+
+        </div>
+
+    </body>
 </html>
