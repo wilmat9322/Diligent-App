54688c273c49e2346b4e49d19f52a9a01fc4a420
wilmat9322 <prisa15.matos@gmail.com>
2021-02-08 20:35:20 -0400

    added more code

---
+++ DiligentApp-repo/html/authenticate-first.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Welcome to Diligent Technologies</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+    <div class="login-box">
+    <img src ="images/user.png" class = "avatar">
+  </body>
+</html>
