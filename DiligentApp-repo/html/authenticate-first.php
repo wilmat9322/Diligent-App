3c2329b095206af966501197b41970d0bedb9776
wilmat9322 <prisa15.matos@gmail.com>
2021-02-08 20:32:16 -0400

    LoginBox empleados casi terminado

--- DiligentApp-repo/html/authenticate-first.php
+++ DiligentApp-repo/html/authenticate-first.php
@@ -2,23 +2,25 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title> Welcome to Diligent Technologies</title>
+    <title> Welcome to DiligentApp</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
   <body>
   <div class="login-box">
-  <img src ="images/user.png" class = "avatar">
-    <h1>Welcome to Diligent Technologies</h1>
+  <img src ="images/AppLogo4.png" class = "avatar">
+    <h1>Welcome to DiligentApp</h1>
       <form>
         <p>Company Code</p>
-        <input type="text" name="username" placeholder="Enter Employee Code">
+        <input type="text" name="username" placeholder="Enter Company Code">
         <p>Employee Code</p>
         <input type="text" name="username" placeholder="Enter Employee Code">
         <p>Password</p>
         <input type="password" name="password" placeholder="Enter Password">
         <input type="submit" name="submit" value="Login">
+        <nav>
         <a href="#">Forgot Password?</a>
         <a href="#">Register here!</a>
+        </nav>
 
       </form>
 
