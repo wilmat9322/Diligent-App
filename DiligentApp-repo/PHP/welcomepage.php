24efad83539c76bc53a1434fe0d81465c1d3c384
wilmat9322 <prisa15.matos@gmail.com>
2021-02-13 22:11:02 -0400

    Avance p ágina principal

--- DiligentApp-repo/PHP/welcomepage.php
+++ DiligentApp-repo/PHP/welcomepage.php
@@ -5,6 +5,8 @@
     <title>Welcome to DiligentApp!</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:ital,wght@1,300&display=swap" rel="stylesheet">
 
   </head>
   <body>
@@ -13,28 +15,31 @@
     <?php include 'CSS/home.css'; ?>
     </style>
 
+      <input type="checkbox" id="check">
       <!--Aquí empieza el header de la aplicación-->
       <header>
         <div>
+          <label for="check">
+            <i class="fas fa-bars" id="menu_btn"></i>
+          </label>
           <a href="#" class="logout">Logout</a>
           <a href="#" class="show_profile">My profile</a>
           <img src="images/Hombre.png" class="profile_image" alt"">
+          <img src="images/Negocio.png" class="negocio" alt="">
           <center>
-          <img src="" class="Logo-Supermercados" alt="" width="20px" height="20px">
-          </center>
+          <h1>Employee Management  |  Automatic Payrolls </h1>
+        </center>
         </div>
       </header>
       <!--Aquí termina el header-->
 
       <!-- Aquí empieza el menu-->
         <div class="menu-slide">
-          <center>
-          <img src="AppLogo3.png" class="Logo-DiligentApp" alt=""> <!-- **Nota** Quiero crear un Div para que el logo del menu y
+          <img src="images/Admin.png" class="Logo-DiligentApp" alt=""> <!-- **Nota** Quiero crear un Div para que el logo del menu y
                                                                         los elementos del menu tengan background colors diferentes -->
-          </center>
-          <center>
-          <h2>Administrator</h2>
-        </center>
+
+          <h4>Administrator</h4>
+
           <a href="#"><i class="far fa-address-card"></i><span>My profile</span></a>
           <a href="#"><i class="far fa-window-maximize"></i><span>Dashboard</span></a>
           <a href="#"><i class="fas fa-users"></i><span>Manage Employees</span></a>
@@ -45,8 +50,12 @@
           <a href="#"><i class="fas fa-bed"></i><span>Leave</span></a>
           <a href="#"><i class="fas fa-user-clock"></i><span>Attendance</span></a>
           <a href="#"><i class="far fa-file-alt"></i><span>Documents and Policies</span></a>
+          <footer>
+          <h6>Powered by Diligent Technologies</h6>
+        </footer>
         </div>
       <!--Aquí termina el menu-->
+      <div class="contenido"></div>
 
   </body>
 </html>
