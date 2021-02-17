114c70076db1596defd27b81edfd2e4baa167a48
wilmat9322 <prisa15.matos@gmail.com>
2021-02-16 21:21:24 -0400

    avances

--- DiligentApp-repo/PHP/authenticate.php
+++ DiligentApp-repo/PHP/authenticate.php
@@ -1,34 +1,53 @@
+<!----- Programa hecho por: William S. Matos Díaz
+     Número de estudiante: B00539322
+     Curso: COMP 4400 Desarrollo e Implementación de Sistemas
+     Profesor: Ing. Rafael Muñoz ----->
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+                    <!----- Lista de Metas que considero importantes ----->
     <meta charset="utf-8">
-    <title> Welcome to DiligentApp</title>
+    <meta name="description" content="Diligent App es una aplicación de manejo de manejo de empelados y nóminas desarrollada
+                                      por Diligent Technologies. Diligent Technologies ha sido una empresa con más de 10 años
+                                      de experiencia, encargada de proveer soluciones a grandes negocios a nivel de Puerto Rico.
+                                      Es destacada por hacer aplicaciones para computadoras que ayudan... "/>
+    <meta name="keywords" content="DiligentApp, Employee Management, Payroll"/>
+    <meta name="author" content="Diligent Technologies"/>
 
+    <title> Login here!</title>
   </head>
+
   <body>
+                    <!----- Referencia al file del diseño de la página de Login ----->
     <style>
     <?php include 'CSS/Login.css'; ?>
     </style>
+
+                      <!----- Comienza el LoginBox ----->
     <div>
-      <img src="Fondo.jpg" class="Fondo">
-  <div class="login-box"> <!-- div del css del LoginBox admin -->
-  <img src ="images/Admin.png" class = "Logo"> <!-- Logo de DiligentApp Admin -->
-    <h1>Welcome to DiligentApp</h1>
-      <form>
+      <img src="images/Fondo.jpg" class="Fondo"> <!-- Fondo del loginbox -->
+    <div class="login-box"> <!-- Caja de Login -->
+      <img src ="images/Admin.png" class = "Logo"> <!-- Logo de DiligentApp -->
+      <h1>Welcome to DiligentApp</h1>
+
+                      <!----- Información del usuario ----->
+    <form>
         <p>Company Code</p>
-        <input type="text" name="username" placeholder="Enter Company Code">
+          <input type="text" name="username" placeholder="Enter Company Code">
         <p>Employee Code</p>
-        <input type="text" name="username" placeholder="Enter Employee Code">
+          <input type="text" name="username" placeholder="Enter Employee Code">
         <p>Password</p>
-        <input type="password" name="password" placeholder="Enter Password">
-        <input type="submit" name="submit" value="Login">
+          <input type="password" name="password" placeholder="Enter Password">
+          <input type="submit" name="submit" value="Login">
+
+                      <!----- Lista de nav como Forgot Password y Register Here! ----->
         <nav>
-        <a href="#">Forgot Passworkd?</a>
-        <a href="#">Register here!</a>
-      </nav>
-      </form>
-
-        </div>
-
-    </body>
+          <a href="#">Forgot Passworkd?</a>
+          <a href="#">Register here!</a>
+        </nav>
+                      <!----- Termina el LoginBox ----->
+    </form>
+    </div>
+  </body>
 </html>
--- DiligentApp-repo/PHP/welcome_admin.php
+++ DiligentApp-repo/PHP/welcome_admin.php
@@ -1,23 +1,43 @@
+<!----- Programa hecho por: William S. Matos Díaz
+     Número de estudiante: B00539322
+     Curso: COMP 4400 Desarrollo e Implementación de Sistemas
+     Profesor: Ing. Rafael Muñoz ----->
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+
+      <!----- Lista de Metas que considero importantes ----->
     <meta charset="utf-8">
+    <meta name="description" content="Diligent App es una aplicación de manejo de manejo de empelados y nóminas desarrollada
+                                      por Diligent Technologies. Diligent Technologies ha sido una empresa con más de 10 años
+                                      de experiencia, encargada de proveer soluciones a grandes negocios a nivel de Puerto Rico.
+                                      Es destacada por hacer aplicaciones para computadoras que ayudan... "/>
+    <meta name="keywords" content="DiligentApp, Employee Management, Payroll"/>
+    <meta name="author" content="Diligent Technologies"/>
+
     <title>Welcome to DiligentApp!</title>
 
+      <!----- Fonts y Símbolos que utilicé para la aplicación ----->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:ital,wght@1,300&display=swap" rel="stylesheet">
-
   </head>
+
   <body>
 
+      <!----- Referencia al file del diseño de la app ----->
     <style>
-    <?php include 'CSS/home.css'; ?>
+    <?php include 'CSS/admin.css'; ?>
     </style>
 
+      <!----- Utilicé un input tipo checkbox para el botón de menú.
+              Se utilizó para funcionalidad y se desapareció con un display: none ----->
       <input type="checkbox" id="check">
-      <!--Aquí empieza el header de la aplicación-->
+
+      <!----- Aquí empieza el header de la aplicación ----->
       <header>
+
         <div>
           <label for="check">
             <i class="fas fa-bars" id="menu_btn"></i>
@@ -31,15 +51,15 @@
         </center>
         </div>
       </header>
-      <!--Aquí termina el header-->
+      <!----- Aquí termina el header ----->
 
-      <!-- Aquí empieza el menu-->
+      <!----- Aquí empieza el menu ----->
         <div class="menu-slide">
-          <img src="images/Admin.png" class="Logo-DiligentApp" alt=""> <!-- **Nota** Quiero crear un Div para que el logo del menu y
-                                                                        los elementos del menu tengan background colors diferentes -->
+          <img src="images/Admin.png" class="Logo-DiligentApp" alt="">
 
           <h4>Administrator</h4>
 
+      <!----- Lista de las herramientas con sus símbolos en el menú ----->
           <a href="#"><i class="far fa-address-card"></i><span>My profile</span></a>
           <a href="#"><i class="far fa-window-maximize"></i><span>Dashboard</span></a>
           <a href="#"><i class="fas fa-users"></i><span>Manage Employees</span></a>
@@ -54,7 +74,9 @@
           <h6>Powered by Diligent Technologies</h6>
         </footer>
         </div>
-      <!--Aquí termina el menu-->
+      <!----- Aquí termina el menú ----->
+
+      <!----- Futuro contenido de cada herramienta ----->
       <div class="contenido"></div>
 
   </body>
